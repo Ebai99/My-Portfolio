c671670452a745c1544819ec2c9857b214e4547f
Ebai99 <abibaron99@gmail.com>
2025-08-13 06:42:32 +0100

    Adding a page loader
    using HTML, CSS and JavaScript

--- docs/script/main.js
+++ docs/script/main.js
@@ -1,11 +1,21 @@
+// Function to open the navigation menu
 function openMenu() {
   const nav = document.querySelector("nav");
   nav.classList.remove("closed");
   nav.classList.add("open");
 }
 
+// Function to close the navigation menu
 function closeMenu() {
   const nav = document.querySelector("nav");
   nav.classList.remove("open");
   nav.classList.add("closed");
 }
+
+// JavaScript to hide the loading screen when the page is fully loaded
+window.addEventListener("load", function () {
+  const loader = document.getElementById("loader");
+  setTimeout(() => {
+    loader.classList.add("loader-hidden");
+  }, 500); // Optional delay for smoother transition
+});
